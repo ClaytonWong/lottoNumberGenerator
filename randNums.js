4ab1532bb2e13616884681f08848f688170a33bb
Clayton <c_w_serious-github@yahoo.com.au>
2019-05-05 18:16:15 +1000

    can make array of integers

--- randNums.js
+++ randNums.js
@@ -4,8 +4,18 @@ function randomIntFromInterval(min, max)
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Create array of integers from minCount to maxCount
+let intsArray = [];
+let mincount = 1;
+let maxCount = 2;
+
+for(let count = mincount; count <= maxCount; count++) {
+  intsArray.push(count);
+}
+
+console.log(intsArray);
+
 let min = 1;
 let max = 3;
 let myRandNum = randomIntFromInterval(min, max);
 
-console.log(myRandNum);
