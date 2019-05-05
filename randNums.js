288315537ba4d179aa212fb56d6ce47fe11f34ed
Clayton <c_w_serious-github@yahoo.com.au>
2019-05-05 22:16:53 +1000

    can get multiple random numbers from ints array

--- randNums.js
+++ randNums.js
@@ -4,25 +4,48 @@ function randomIntFromInterval(min, max)
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Create array of integers from minCount to maxCount
-let intsArray = [];
-let mincount = 1;
-let maxCount = 2;
+/*
+Function to pick random integers from an array of integers in order
+from mincount to maxcount
+*/
+function randomIntsFromIntsArray(minCount, maxCount, NumOfRandIntsNeeded) {
+  // Create array of integers from minCount to maxCount
+  let intsArray = [];
+  
+  for(let count = minCount; count <= maxCount; count++) {
+    intsArray.push(count);
+  }
 
-for(let count = mincount; count <= maxCount; count++) {
-  intsArray.push(count);
+  const minIndex = 0;
+  let maxIndex;
+  let randIndex;
+  let copyOfRandInt;
+  let randIntsChosen = [];
+
+  while(NumOfRandIntsNeeded > 0) {
+    maxIndex = intsArray.length - 1;
+    
+    // Pick a random index from intsArray
+    randIndex = randomIntFromInterval(minIndex, maxIndex);
+
+    // Get copy of integer at previously generated index
+    copyOfRandInt = intsArray[randIndex];
+
+    // Add copyOfRandInt to randIntsChosen
+    randIntsChosen.push(copyOfRandInt);
+
+    // Remove integer from array
+    intsArray.splice(randIndex, 1);
+
+    console.log(`randIndex: ${randIndex}`);
+    console.log(`copyOfRandInt: ${copyOfRandInt}`);
+    console.log(`intsArray: ${intsArray}`);
+    console.log(`---------------`);
+
+    NumOfRandIntsNeeded--;
+  }
+
+  return randIntsChosen;
 }
 
-// Pick a random index from intsArray
-let minIndex = 0;
-let maxIndex = intsArray.length - 1;
-let myRandNum = randomIntFromInterval(minIndex, maxIndex);
-
-// Get copy of integer at previously generated index
-let copyOfRandInt = intsArray[myRandNum];
-
-// Remove integer from array
-intsArray.splice(myRandNum, 1);
-console.log(`myRandNum/Random Index: ${myRandNum}`);
-console.log(`copyOfRandInt: ${copyOfRandInt}`);
-console.log(`intsArray: ${intsArray}`);
+console.log("Random ints chosen:", randomIntsFromIntsArray(1, 3, 2), "*");
