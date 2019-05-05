1fb75dd8e5fedd72b86f9f19ce936ffd65ad0ebd
Clayton <c_w_serious-github@yahoo.com.au>
2019-05-05 15:05:14 +1000

    Can generate random integer between min and max

--- randNums.js
+++ randNums.js
@@ -1,4 +1,11 @@
-// Create random number between 0 and 1
-let myRandNum = Math.random();
+// Function to generate random number between min and max inclusive
+function randomIntFromInterval(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let min = 1;
+let max = 3;
+let myRandNum = randomIntFromInterval(min, max);
 
 console.log(myRandNum);
